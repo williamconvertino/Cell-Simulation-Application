a33352e674bc41c0a3bfa5eb30d7f71445e74a53
alexis.cruzayala <adc94@duke.edu>
2021-10-15 01:48:04 -0400

    Added functionality to logic class for reading CSV and Sim files

--- src/cellsociety/logic/Logic.java
+++ src/cellsociety/logic/Logic.java
@@ -1,12 +1,69 @@
 package cellsociety.logic;
 
+import cellsociety.errors.FileNotFoundError;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.Scanner;
+
 public class Logic {
     private Grid grid;
+    HashMap<String, String> metadata;
 
     public Logic(int width, int height){
         grid = new Grid(width, height);
-
     }
+
+
+    public void initializeWithCSVFile(String file){
+        try{
+            //parsing a CSV file into Scanner class constructor
+            Scanner sc = new Scanner(new File(file));
+            sc.useDelimiter(",");//sets the delimiter pattern
+            //width and height are the first two values in the csv
+            int width = sc.nextInt();
+            int height = sc.nextInt();
+            this.grid = new Grid(width, height);
+            int i = 0, j = 0;
+            while (sc.hasNext())  //returns a boolean value
+            {
+                grid.setCell(i% grid.getWidth(), j% grid.getHeight(), sc.nextInt());  //find and returns the next complete token from this scanner
+                j++;
+                if(j%grid.getHeight() == 0) i++;
+            }
+            sc.close();  //closes the scanner
+
+            grid.updateGrid();
+        } catch (FileNotFoundException e){
+            //TODO do some error checking here
+        }
+    }
+
+    public void initializeWithSimFile(String file){
+        Properties prop = readPropertiesFile(file);
+        for (final String name: prop.stringPropertyNames())
+            metadata.put(name, prop.getProperty(name));
+    }
+
+    private Properties readPropertiesFile(String fileName){
+        FileInputStream fis;
+        Properties prop = null;
+        try {
+            fis = new FileInputStream(fileName);
+            prop = new Properties();
+            prop.load(fis);
+            fis.close();
+        } catch(FileNotFoundException fnfe) {
+        } catch(IOException ioe) {
+        }
+        return prop;
+    }
+
 
 
 }
