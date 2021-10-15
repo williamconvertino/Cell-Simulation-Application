0b1e4ed4b44f741e7886b0eb3687b0b4fd260ba4
alexis.cruzayala <adc94@duke.edu>
2021-10-15 16:25:33 -0400

    added bounds checking to grid access neighborhood

--- src/cellsociety/logic/Grid.java
+++ src/cellsociety/logic/Grid.java
@@ -49,15 +49,58 @@ public class Grid {
     //of the cell at (x,y)
     public int[] getAllNeighbors(int x, int y) {
         int[] result = new int[8];
-        //TODO do some bounds checking here
-        result[0] = currentGrid[x - 1][y - 1];
-        result[1] = currentGrid[x - 1][y];
-        result[2] = currentGrid[x - 1][y + 1];
-        result[3] = currentGrid[x][y - 1];
-        result[4] = currentGrid[x][y + 1];
-        result[5] = currentGrid[x + 1][y - 1];
-        result[6] = currentGrid[x + 1][y];
-        result[7] = currentGrid[x + 1][y + 1];
+        if(x == 0){
+            if((y != 0 && y != width - 1)){
+                result[0] = -1;
+                result[1] = -1;
+                result[2] = -1;
+            } else if (y == 0){
+                result[0] = currentGrid[x - 1][y - 1];
+                result[1] = -1;
+                result[2] = -1;
+            } else if (y == width - 1){
+                result[0] = -1;
+                result[1] = -1;
+                result[2] = currentGrid[x - 1][y + 1];
+            }
+        }else {
+            result[0] = currentGrid[x - 1][y - 1];
+            result[1] = currentGrid[x - 1][y];
+            result[2] = currentGrid[x - 1][y + 1];
+        }
+
+
+        if(x == width - 1){
+            if((y != 0 && y != height - 1)){
+                result[5] = -1;
+                result[6] = -1;
+                result[7] = -1;
+            } else if (y == 0){
+                result[5] = currentGrid[x + 1][y - 1];
+                result[6] = -1;
+                result[7] = -1;
+            } else if (y == width - 1){
+                result[5] = -1;
+                result[6] = -1;
+                result[7] = currentGrid[x + 1][y + 1];
+            }
+        } else{
+            result[5] = currentGrid[x + 1][y - 1];
+            result[6] = currentGrid[x + 1][y];
+            result[7] = currentGrid[x + 1][y + 1];
+        }
+
+        if(y == 0){
+            result[3] = currentGrid[x][y - 1];
+            result[4] = -1;
+        } else if(y == height - 1) {
+            result[3] = -1;
+            result[4] = currentGrid[x][y + 1];
+        } else{
+            result[3] = currentGrid[x][y - 1];
+            result[4] = currentGrid[x][y + 1];
+        }
+
 
         return result;
     }
