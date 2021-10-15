a61e335a50d0f331c9f7d87011ce2b86f0e3e2e5
alexis.cruzayala <adc94@duke.edu>
2021-10-15 01:15:35 -0400

    added Grid class and began adding to Logic class

---
+++ src/cellsociety/logic/Grid.java
@@ -0,0 +1,99 @@
+package cellsociety.logic;
+
+import java.util.ArrayList;
+
+public class Grid {
+    private int[][] currentGrid;
+    private int[][] futureGrid;
+    private int width;
+    private int height;
+
+    //constructor that sets the width and height of the current
+    // and previous grid and initializes all values to 0
+    public Grid(int width, int height) {
+        currentGrid = new int[width][height];
+        futureGrid = new int[width][height];
+        this.width = width;
+        this.height = height;
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                currentGrid[i][j] = 0;
+                futureGrid[i][j] = 0;
+            }
+        }
+
+    }
+
+    //constructor that sets the width and height of the current
+    // and previous grid and initializes all values to initialValue
+    public Grid(int width, int height, int initialValue) {
+        currentGrid = new int[width][height];
+        futureGrid = new int[width][height];
+        this.width = width;
+        this.height = height;
+        currentGrid = zeroGrid();
+        futureGrid = zeroGrid();
+    }
+
+    //gets the value of the cell at (x,y)
+    public int getCell(int x, int y) {
+        return currentGrid[x][y];
+    }
+
+    //sets the value of the cell at (x,y)
+    public void setCell(int x, int y, int newVal) {
+        futureGrid[x][y] = newVal;
+    }
+
+    //gets an array of the values of the 8 neighbors
+    //of the cell at (x,y)
+    public int[] getAllNeighbors(int x, int y) {
+        int[] result = new int[8];
+        result[0] = currentGrid[x - 1][y - 1];
+        result[1] = currentGrid[x - 1][y];
+        result[2] = currentGrid[x - 1][y + 1];
+        result[3] = currentGrid[x][y - 1];
+        result[4] = currentGrid[x][y + 1];
+        result[5] = currentGrid[x + 1][y - 1];
+        result[6] = currentGrid[x + 1][y];
+        result[7] = currentGrid[x + 1][y + 1];
+
+        return result;
+    }
+
+    //gets an array of the values of the 4 neighbors
+    //directly above, below, to the left and to the right
+    //of the cell at (x,y)
+    public int[] getFourNeighbors(int x, int y) {
+        int[] result = new int[4];
+        result[0] = currentGrid[x - 1][y];
+        result[1] = currentGrid[x][y - 1];
+        result[2] = currentGrid[x][y + 1];
+        result[3] = currentGrid[x + 1][y];
+        return result;
+    }
+
+    public void updateGrid() {
+        currentGrid = futureGrid;
+        futureGrid = zeroGrid();
+    }
+
+    private int[][] zeroGrid() {
+        int[][] result = new int[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                result[i][j] = 0;
+            }
+        }
+
+        return result;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+}
--- src/cellsociety/logic/Logic.java
+++ src/cellsociety/logic/Logic.java
@@ -1,5 +1,12 @@
 package cellsociety.logic;
 
 public class Logic {
+    private Grid grid;
+
+    public Logic(int width, int height){
+        grid = new Grid(width, height);
+
+    }
+
 
 }
