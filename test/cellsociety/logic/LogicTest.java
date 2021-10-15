1bf917e3b4865d8dbd3cba663a8d5a7e03fbe372
alexis.cruzayala <adc94@duke.edu>
2021-10-15 17:29:34 -0400

    added logic test class

---
+++ test/cellsociety/logic/LogicTest.java
@@ -0,0 +1,19 @@
+package cellsociety.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LogicTest {
+
+    @Test
+    void initializeWithCSVFile() {
+    }
+
+    @Test
+    void initializeWithSimFile() {
+    }
+
+    @Test
+    void getGrid() {
+    }
+}
