968bbdafee73f184e81fba4275e1afe7ff3b7c11
alexis.cruzayala <adc94@duke.edu>
2021-10-16 15:34:47 -0400

    started update test reviewing update function in Gameoflife

--- test/cellsociety/logic/GameOfLifeTest.java
+++ test/cellsociety/logic/GameOfLifeTest.java
@@ -11,10 +11,18 @@ class GameOfLifeTest {
     @BeforeEach
     void setUp(){
         gol = new GameOfLife(100, 100);
+        gol.initializeWithCSVFile("data/game_of_life/blinkers.csv");
+        gol.initializeWithSimFile("data/game_of_life/blinkers.sim");
     }
 
     @Test
     void updateTest(){
+        gol.update();
+        for(int i = 0; i < gol.getGrid().getWidth(); i++){
+            for(int j = 0; j < gol.getGrid().getHeight(); j++){
+                System.out.println(gol.getGrid().getCell(i, j) + ",");
+            }
+        }
 
     }
 }
