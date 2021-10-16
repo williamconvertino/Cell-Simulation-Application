746554e7fc70b551449f6648ce89873c900c0b03
William Convertino <wac20@duke.edu>
2021-10-16 14:08:10 -0400

    Updated Controller class with some relevant class variables and a skeleton for a few of its future methods.

--- src/cellsociety/Controller.java
+++ src/cellsociety/Controller.java
@@ -1,6 +1,40 @@
 package cellsociety;
 
+import cellsociety.display.Display;
+import cellsociety.io.IOHandler;
+import cellsociety.logic.Logic;
+
+/**
+ * Organizes and runs the different parts of the Cell Society
+ * program, allowing the Display, Logic, IO handling, and Error
+ * handling to communicate with each other.
+ *
+ * @author William Convertino
+ * @since 0.0.1
+ */
 public class Controller {
 
+  //The current algorithm with which the grid should be updated.
+  private Logic myAlgorithm;
+
+  //The current display class of our program.
+  private Display myDisplay;
+
+  //The current IOHandler of our program.
+  private IOHandler myIOHandler;
+
+  /**
+   *
+   */
+  public Controller() {
+
+  }
+
+  /**
+   *
+   */
+  public void update() {
+
+  }
 
 }
