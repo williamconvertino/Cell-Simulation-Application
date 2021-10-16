36061943426379eaab77d4fd2f28ee884be768e4
William Convertino <wac20@duke.edu>
2021-10-16 19:25:16 -0400

    Moved CVS and SIM file reading methods to their own classes.

--- src/cellsociety/io/CVSFileReader.java
+++ src/cellsociety/io/CVSFileReader.java
@@ -1,5 +1,56 @@
 package cellsociety.io;
 
+import cellsociety.logic.Grid;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+/**
+ * A class to read CVS files.
+ *
+ */
 public class CVSFileReader {
+
+  public static Grid readFile(String fileName) {
+
+    Grid myGrid;
+
+    try{
+      // Create an object of filereader
+      // class with CSV file as a parameter.
+      FileReader filereader = new FileReader(fileName);
+
+      // create csvReader object passing
+      // file reader as a parameter
+      CSVReader csvReader = new CSVReader(filereader);
+      int i = 0, j = 0;
+
+      String[] nextRecord;
+      nextRecord = csvReader.readNext();
+      //make a dimensions array to just read the width and height first
+      int[] dimensions = new int[2];
+      for (String cell : nextRecord) {
+        dimensions[i] = Integer.parseInt(cell); i++;
+      }
+      //set i =0 because we'll be using it to iterate
+      i = 0;
+      //dimensions[0] is width and dimensions[1] is height
+      myGrid = new Grid(dimensions[0], dimensions[1]);
+      while ((nextRecord = csvReader.readNext()) != null) {
+        for (String cell : nextRecord) {
+          myGrid.setCell(i% myGrid.getWidth(), j% myGrid.getHeight(), Integer.parseInt(cell));
+          j++;
+        }
+        i++;
+      }
+      myGrid.updateGrid();
+      return myGrid;
+    } catch (Exception e){   //TODO: Add better error handling.
+      return null;
+    }
+  }
 
 }
--- src/cellsociety/io/SIMFileReader.java
+++ src/cellsociety/io/SIMFileReader.java
@@ -1,5 +1,39 @@
 package cellsociety.io;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 public class SIMFileReader {
+
+  public static Map<String,String> getMetadataFromFile(String file) {
+
+    Map metadata = new HashMap();
+
+    Properties prop = readPropertiesFile(file);
+    for (final String name : prop.stringPropertyNames()) {
+      metadata.put(name, prop.getProperty(name));
+    }
+
+    return metadata;
+  }
+
+  public static Properties readPropertiesFile(String fileName){
+    FileInputStream fis;
+    Properties prop = null;
+    try {
+      fis = new FileInputStream(fileName);
+      prop = new Properties();
+      prop.load(fis);
+      fis.close();
+    } catch(FileNotFoundException fnfe) {
+    } catch(IOException ioe) {
+    }
+    return prop;
+  }
 
 }
