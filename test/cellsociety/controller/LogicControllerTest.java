a0eeebf744d3dca3d4512172bff47775e4e72ec3
alexis.cruzayala <adc94@duke.edu>
2021-10-19 12:35:12 -0400

    changed paths for csvs in sims

---
+++ test/cellsociety/controller/LogicControllerTest.java
@@ -0,0 +1,28 @@
+package cellsociety.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogicControllerTest {
+
+    @Test
+    void initializeFromFile() {
+    }
+
+    @Test
+    void getActiveGrid() {
+    }
+
+    @Test
+    void pauseSimulation() {
+    }
+
+    @Test
+    void playSimulation() {
+    }
+
+    @Test
+    void update() {
+    }
+}
