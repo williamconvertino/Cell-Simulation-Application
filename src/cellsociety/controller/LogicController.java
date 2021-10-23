3fbed00e21efa3bba4267f01a02461f4fd314e19
alexis.cruzayala <adc94@duke.edu>
2021-10-23 16:36:44 -0400

    adding logic for fire spreader

--- src/cellsociety/controller/LogicController.java
+++ src/cellsociety/controller/LogicController.java
@@ -118,8 +118,8 @@ public class LogicController {
   }
 
   //Returns a new FireSpreading simulation.
-  public Simulation FireSpreading(Grid grid, Map<String, String> metadata) {
-    return new FireSpreading(grid, metadata);
+  public Simulation FireSpreading(Grid grid, Map<String, String> metadata, double probCatch) {
+    return new FireSpreading(grid, metadata, probCatch);
   }
 
   //Returns a new WaTorWorld simulation.
