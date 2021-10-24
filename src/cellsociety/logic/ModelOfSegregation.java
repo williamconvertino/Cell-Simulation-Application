644314195f8aa1aa91ed54b256bbd7ba21e3b36d
alexis.cruzayala <adc94@duke.edu>
2021-10-23 23:22:36 -0400

    added new configuration for model of segregation

--- src/cellsociety/logic/ModelOfSegregation.java
+++ src/cellsociety/logic/ModelOfSegregation.java
@@ -1,10 +1,13 @@
 package cellsociety.logic;
 
 import cellsociety.errors.MissingSimulationArgumentError;
+
+import java.util.Collections;
 import java.util.Map;
 
 public class ModelOfSegregation extends Simulation{
 
+  private double satisfactionRate;
   /**
    * Constructs a new Simulation with a specified starting Grid and a Map of simulation-specific data
    * values.
@@ -17,10 +20,22 @@ public class ModelOfSegregation extends Simulation{
   public ModelOfSegregation(Grid grid, Map<String, String> metadata)
       throws MissingSimulationArgumentError {
     super(grid, metadata);
+    satisfactionRate = Double.parseDouble(metadata.get("SatisfactionRate"));
   }
 
   @Override
   public void update() {
-
+    for (int x = 0; x < getGrid().getWidth(); x++) {
+      for (int y = 0; y < getGrid().getHeight(); y++) {
+        if (Collections.frequency(getGrid().getFourNeighbors(x, y), 2)/4 < satisfactionRate) {
+          getGrid().setCell(x, y, 2);
+        } else if(getGrid().getCell(x, y) == 2) {
+          getGrid().setCell(x, y, 0);
+        }else {
+          getGrid().setCell(x, y, getGrid().getCell(x, y));
+        }
+      }
+    }
+    getGrid().updateGrid();
   }
 }
