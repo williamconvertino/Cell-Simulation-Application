2dba2c6b264ff76a1ecfc4a7e0d58c95a1ea203a
alexis.cruzayala <adc94@duke.edu>
2021-10-25 23:25:02 -0400

    added simple implementation of percolation

--- src/cellsociety/logic/simulations/Percolation.java
+++ src/cellsociety/logic/simulations/Percolation.java
@@ -27,12 +27,10 @@ public class Percolation extends Simulation {
   public void update() {
     for (int x = 0; x < getGrid().getWidth(); x++) {
       for (int y = 0; y < getGrid().getHeight(); y++) {
-        if (getGrid().getFourNeighbors(x, y).get(0) == 1) {
-          getGrid().setCell(x, y, 0);
-        } else if (Collections.frequency(getGrid().getAllNeighbors(x, y), 1) == 3) {
+        if (getGrid().getFourNeighbors(x, y).get(0) == 1 && getGrid().getCell(x, y) == 0) {
           getGrid().setCell(x, y, 1);
-        } else if (Collections.frequency(getGrid().getAllNeighbors(x, y), 1) >= 4) {
-          getGrid().setCell(x, y, 0);
+        } else if (getGrid().getFourNeighbors(x, y).get(0) == -1 && getGrid().getCell(x, y) == 0) {
+          getGrid().setCell(x, y, 1);
         } else {
           getGrid().setCell(x, y, getGrid().getCell(x, y));
         }
