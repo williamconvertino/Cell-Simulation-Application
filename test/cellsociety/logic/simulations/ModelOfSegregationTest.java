787fc4ed21d545cbf20ae0e2ed3eb68031a2f015
alexis.cruzayala <adc94@duke.edu>
2021-10-27 00:57:38 -0400

    adding modelofsegregationtest

--- test/cellsociety/logic/simulations/ModelOfSegregationTest.java
+++ test/cellsociety/logic/simulations/ModelOfSegregationTest.java
@@ -1,37 +1,44 @@
 package cellsociety.logic.simulations;
 
+import cellsociety.io.CSVFileReader;
+import cellsociety.io.SIMFileReader;
+import cellsociety.logic.grid.Grid;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ModelOfSegregationTest {
-/*
+
     private ModelOfSegregation mos;
 
     @Test
-    void updateTest(){
+    void linesTest(){
         try {
-            Grid grid = CSVFileReader.readFile("data/segregation_model/lines.csv");
+            Grid grid = new Grid(CSVFileReader.readFile(new File("data/segregation_model/lines.csv")));
             HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/segregation_model/lines.sim"));
-            mos = new ModelOfSegregation(grid, metadata);
+            mos = new ModelOfSegregation(grid.getCellStates(), metadata);
             mos.update();
             int x = 0;
             int y = 0;
-            assertEquals(1, mos.getGrid().getCell(x, y));
+            assertEquals(1, mos.getGrid().getCell(x, y).getState());
             x = 0;
             y = 1;
-            assertEquals(1, mos.getGrid().getCell(x, y));
+            assertEquals(1, mos.getGrid().getCell(x, y).getState());
             x = 1;
             y = 0;
-            assertEquals(1, mos.getGrid().getCell(x, y));
+            assertEquals(1, mos.getGrid().getCell(x, y).getState());
             x = 2;
             y = 1;
-            assertEquals(1, mos.getGrid().getCell(x, y));
+            assertEquals(1, mos.getGrid().getCell(x, y).getState());
             x = 1;
             y = 2;
-            assertEquals(1, mos.getGrid().getCell(x, y));
+            assertEquals(1, mos.getGrid().getCell(x, y).getState());
         } catch (Exception e) {
             fail("File Not Found");
         }
 
     }
-*/
 }
