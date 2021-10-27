4c8baeaafa082e64b2c3f3cd97ef7f0c89e56071
Quentin MacFarlane <qtm@duke.edu>
2021-10-27 02:04:36 -0400

    rough draft for using nextGrid instead of currentGrid

--- src/cellsociety/logic/simulations/WaTorWorld.java
+++ src/cellsociety/logic/simulations/WaTorWorld.java
@@ -12,15 +12,15 @@ import java.util.*;
 
 public class WaTorWorld extends Simulation {
 
-    private int[][] originalGrid;
-    private int energy;
+    private int initEnergy;
     private int reproductionTime;
+    private int energyPerFish;
 
     public WaTorWorld(int[][] grid, Map<String, String> metadata) throws MissingSimulationArgumentError {
         super(grid, metadata);
-        originalGrid = grid;
-        energy = Integer.parseInt(metadata.get("Energy"));
+        initEnergy = Integer.parseInt(metadata.get("InitialEnergy"));
         reproductionTime = Integer.parseInt(metadata.get("ReproductionTime"));
+        energyPerFish = Integer.parseInt(metadata.get("EnergyPerFish"));
         changeToWatorCells(); // changes the grid to use WaTorCells instead of regular Cells
     }
 
@@ -32,7 +32,7 @@ public class WaTorWorld extends Simulation {
                     getGrid().setCell(r, c, new WaTorCell(r,c,getGrid().getCellState(r,c), new Fish()));
                 }
                 else if (getGrid().getCell(r,c).getState() == 2) { // if the cell's state is a 2 (meaning it's a shark)
-                    getGrid().setCell(r, c, new WaTorCell(r,c,getGrid().getCellState(r,c), new Shark(energy)));
+                    getGrid().setCell(r, c, new WaTorCell(r,c,getGrid().getCellState(r,c), new Shark(initEnergy)));
                 }
             }
         }
@@ -41,7 +41,7 @@ public class WaTorWorld extends Simulation {
     @Override
     protected void updateNextGridFromCell(Cell cell) {
         // if the cell right now is just water, or if the original cell was just water, do nothing
-        if (cell.getState() == 0 || originalGrid[cell.getRow()][cell.getColumn()] == 0) {
+        if (cell.getState() == 0) {
             return;
         } else if (cell.getState() == 1) { // if the cell is a fish
             // increase the time survived for the fish by 1
@@ -79,17 +79,23 @@ public class WaTorWorld extends Simulation {
             // first, figure out where to move the fish (random spot from the adjacent water spots)
             Collections.shuffle(neighbors);
 
-            // move the fish to the spot identified
-            currentGrid.setCell(neighbors.get(0).getRow(), neighbors.get(0).getColumn(), ((WaTorCell) cell));
-            // sets the previous spot where the fish just was to empty water
-            currentGrid.setCellState(cell.getRow(), cell.getColumn(), 0);
+            // check to see if the nextGrid has an empty water spot for the cell we want to move to
+            if (nextGrid.getCell(neighbors.get(0).getRow(), neighbors.get(0).getColumn()).getState() == 0) {
+                // move the fish to the spot identified
+                nextGrid.setCell(neighbors.get(0).getRow(), neighbors.get(0).getColumn(), ((WaTorCell) cell));
+                // sets the previous spot where the fish just was to empty water
+//                nextGrid.setCellState(cell.getRow(), cell.getColumn(), 0);
 
-            // reproduce a new fish if the fish has survived for "reproductionTime" amount of time
-            if (((WaTorCell) cell).getAnimal().getTimeSurvived() == reproductionTime) {
-                currentGrid.setCell(cell.getRow(), cell.getColumn(),
-                        new WaTorCell(cell.getRow(),cell.getColumn(),
-                                getGrid().getCellState(cell.getRow(),cell.getColumn()), new Fish()));
+                // reproduce a new fish if the fish has survived for "reproductionTime" amount of time
+                if (((WaTorCell) cell).getAnimal().getTimeSurvived() == reproductionTime) {
+                    nextGrid.setCell(cell.getRow(), cell.getColumn(),
+                            new WaTorCell(cell.getRow(),cell.getColumn(),
+                                    getGrid().getCellState(cell.getRow(),cell.getColumn()), new Fish()));
+                }
             }
+        }
+        else { // if the fish cannot move
+            nextGrid.setCell(cell.getRow(), cell.getColumn(), (WaTorCell) cell);
         }
     }
 
@@ -98,44 +104,54 @@ public class WaTorWorld extends Simulation {
             // first, figure out which fish to eat
             Collections.shuffle(neighborsFish);
 
-            // move the shark to the spot identified
-            currentGrid.setCell(neighborsFish.get(0).getRow(), neighborsFish.get(0).getColumn(), ((WaTorCell) cell));
-            // sets the previous spot where the shark just was to empty water
-            currentGrid.setCellState(cell.getRow(), cell.getColumn(), 0);
+            // checking to see if a shark has occupied the next spot already, or if the fish already moved away
+            if (nextGrid.getCell(neighborsFish.get(0).getRow(), neighborsFish.get(0).getColumn()).getState() != 2
+                    || nextGrid.getCell(neighborsFish.get(0).getRow(), neighborsFish.get(0).getColumn()).getState() == 0) {
+                // move the shark to the spot identified
+                nextGrid.setCell(neighborsFish.get(0).getRow(), neighborsFish.get(0).getColumn(), ((WaTorCell) cell));
+                // sets the previous spot where the shark just was to empty water
+//                nextGrid.setCellState(cell.getRow(), cell.getColumn(), 0);
 
-            Shark currentShark = ((Shark) ((WaTorCell) cell).getAnimal());
-            // increase the energy for the shark by "energy" amount
-            currentShark.setEnergy(currentShark.getEnergy() + energy);
+                Shark currentShark = ((Shark) ((WaTorCell) cell).getAnimal());
+                // increase the energy for the shark by "energy" amount
+                currentShark.setEnergy(currentShark.getEnergy() + energyPerFish);
+            }
         }
         else if (neighborsEmpty.size() > 0) { // if there is an empty water spot next to the shark
             // first, figure out which water spot to move to
             Collections.shuffle(neighborsEmpty);
 
-            // move the shark to the spot identified
-            currentGrid.setCell(neighborsEmpty.get(0).getRow(), neighborsEmpty.get(0).getColumn(), ((WaTorCell) cell));
-            // sets the previous spot where the shark just was to empty water
-            currentGrid.setCellState(cell.getRow(), cell.getColumn(), 0);
+            // check to see if the nextGrid has an empty water spot for the cell we want to move to
+            if (nextGrid.getCell(neighborsFish.get(0).getRow(), neighborsFish.get(0).getColumn()).getState() == 0) {
+                // move the shark to the spot identified
+                nextGrid.setCell(neighborsEmpty.get(0).getRow(), neighborsEmpty.get(0).getColumn(), ((WaTorCell) cell));
+                // sets the previous spot where the shark just was to empty water
+//                nextGrid.setCellState(cell.getRow(), cell.getColumn(), 0);
+            }
+        }
+        else { // if there is no fish AND no empty water for the shark to move (basically the shark is surrounded by 4 sharks)
+            nextGrid.setCell(cell.getRow(), cell.getColumn(), (WaTorCell) cell);
         }
     }
 
     private void checkEnergy(Shark currentShark, List<Cell> neighborsEmpty, Cell cell) {
         // if the shark ran out of energy and moved, set it to empty water space
         if (currentShark.getEnergy() == 0 && neighborsEmpty.size() > 0) {
-            currentGrid.setCellState(neighborsEmpty.get(0).getRow(), neighborsEmpty.get(0).getColumn(), 0);
+            nextGrid.setCellState(neighborsEmpty.get(0).getRow(), neighborsEmpty.get(0).getColumn(), 0);
         }
         // else if the shark ran out of energy but didn't move, set it to empty water space
         else if (currentShark.getEnergy() == 0 && neighborsEmpty.isEmpty()) {
-            currentGrid.setCellState(cell.getRow(), cell.getColumn(), 0);
+            nextGrid.setCellState(cell.getRow(), cell.getColumn(), 0);
         }
-        else { // if shark didn't run out of energy, increase the time survived by 1
+        else if (currentShark.getEnergy() != 0){ // if shark didn't run out of energy, increase the time survived by 1
             // increase the time survived for the shark by 1
             ((WaTorCell) cell).getAnimal().setTimeSurvived(((WaTorCell) cell).getAnimal().getTimeSurvived() + 1);
 
             // reproduce a new shark if the shark has survived for "reproductionTime" amount of time
             if (((WaTorCell) cell).getAnimal().getTimeSurvived() == reproductionTime) {
-                currentGrid.setCell(cell.getRow(), cell.getColumn(),
+                nextGrid.setCell(cell.getRow(), cell.getColumn(),
                         new WaTorCell(cell.getRow(),cell.getColumn(),
-                                getGrid().getCellState(cell.getRow(),cell.getColumn()), new Shark(energy)));
+                                getGrid().getCellState(cell.getRow(),cell.getColumn()), new Shark(initEnergy)));
             }
         }
     }
