38e01e40c212750466d9d0b6e0e8619e5f576199
William Convertino <wac20@duke.edu>
2021-10-28 12:48:09 -0400

    Added getCell method and updated coordiantes in Grid class

--- src/cellsociety/logic/grids/Grid.java
+++ src/cellsociety/logic/grids/Grid.java
@@ -1,9 +1,26 @@
 package cellsociety.logic.grids;
 
+import cellsociety.logic.cells.Cell;
+import java.util.Map;
+
 public class Grid {
 
+  //A map of the cells in the grid.
+  private Map<Coordinate, Cell> myCells;
 
-  Map<Coordinate, Cell>
+  /**
+   * Returns the value at the specified coordinates, or null if
+   * there is no cell at the coordinates.
+   *
+   * @param r the row coordinate of the cell to get.
+   * @param c the column coordinate of the cell to get.
+   * @return the cell at coordinates (r,c), or null if it does not exist.
+   */
+  public Cell getCell(int r, int c) {
+    Coordinate cellCoords = new Coordinate(r,c);
+    return myCells.getOrDefault(cellCoords, null);
+  }
+
 
 
   //A class to keep track of the coordinate pairs for our map.
@@ -17,6 +34,21 @@ public class Grid {
     public Coordinate(int r, int c) {
       this.r=r;
       this.c=c;
+    }
+
+    //Overrides the equals method to state that 2 coordinates are equal if they share the same values.
+    @Override
+    public boolean equals(Object o) {
+      if (!(o instanceof Coordinate)) return false;
+      Coordinate oCoordinate = (Coordinate) o;
+      return this == o || (this.r == oCoordinate.r && this.c == oCoordinate.c);
+    }
+
+    //Overrides the hashCode method to create a new hashcode based on the coordinates.
+    //This will help distribute our coordinates in the map.
+    @Override
+    public int hashCode() {
+      return (r * 31) + c;
     }
   }
 }
