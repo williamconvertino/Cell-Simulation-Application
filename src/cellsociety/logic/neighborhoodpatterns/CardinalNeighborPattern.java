bdb091a44847a4659352eca0f23c38936da9353d
alexis.cruzayala <adc94@duke.edu>
2021-10-29 15:44:38 -0400

    added the logic for the neighborhood pattern subclasses

--- src/cellsociety/logic/neighborhoodpatterns/CardinalNeighborPattern.java
+++ src/cellsociety/logic/neighborhoodpatterns/CardinalNeighborPattern.java
@@ -3,11 +3,17 @@ package cellsociety.logic.neighborhoodpatterns;
 import cellsociety.logic.grid.Coordinate;
 import cellsociety.logic.shapes.ShapeManager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CardinalNeighborPattern extends NeighborhoodPattern{
     @Override
     public List<Coordinate> getNeighborhoodGroup(Coordinate myCenter, ShapeManager myShape) {
-        return null;
+        ArrayList<Coordinate> result = new ArrayList<>();
+        result.addAll(myShape.getAllNeighborsUp(myCenter));
+        result.addAll(myShape.getAllNeighborsLeft(myCenter));
+        result.addAll(myShape.getAllNeighborsDown(myCenter));
+        result.addAll(myShape.getAllNeighborsRight(myCenter));
+        return result;
     }
 }
--- src/cellsociety/logic/neighborhoodpatterns/CornerNeighborPattern.java
+++ src/cellsociety/logic/neighborhoodpatterns/CornerNeighborPattern.java
@@ -3,16 +3,18 @@ package cellsociety.logic.neighborhoodpatterns;
 import cellsociety.logic.grid.Coordinate;
 import cellsociety.logic.shapes.ShapeManager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CornerNeighborPattern extends NeighborhoodPattern{
 
     @Override
     public List<Coordinate> getNeighborhoodGroup(Coordinate myCenter, ShapeManager myShape) {
-        List<Coordinate> result = myShape.getAllNeighbors(myCenter);
-
-
-
+        ArrayList<Coordinate> result = new ArrayList<>();
+        result.addAll(myShape.getNeighborsUL(myCenter));
+        result.addAll(myShape.getNeighborsUR(myCenter));
+        result.addAll(myShape.getNeighborsDL(myCenter));
+        result.addAll(myShape.getNeighborsDR(myCenter));
         return result;
     }
 }
