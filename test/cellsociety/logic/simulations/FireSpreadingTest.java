b4c3b247247c75535ae776acc3a0519305157fb7
alexis.cruzayala <adc94@duke.edu>
2021-10-30 18:11:08 -0400

    added more firespreadingTests

--- test/cellsociety/logic/simulations/FireSpreadingTest.java
+++ test/cellsociety/logic/simulations/FireSpreadingTest.java
@@ -12,7 +12,6 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static cellsociety.controller.LogicController.INITIAL_STATE_FILE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -27,7 +26,7 @@ class FireSpreadingTest {
             metadata.put("ProbCatch", "1.00");
             Class cls[] = new Class[]{int[][].class, ShapeManager.class};
             ShapeManager sm = (ShapeManager) Class.forName("cellsociety.logic.shapes." + metadata.get("Shape")).getConstructor().newInstance();
-            Object[] params = {CSVFileReader.readFile(new File(metadata.get(INITIAL_STATE_FILE))),
+            Object[] params = {CSVFileReader.readFile(new File(metadata.get("InitialStates"))),
                     sm};
             Grid grid = (Grid) Class.forName("cellsociety.logic.grid." + metadata.get("GridType")).getConstructor(cls).newInstance(params);
             fs = (FireSpreading) FireSpreading.class.getConstructor(Grid.class, NeighborhoodPattern.class, Map.class).newInstance(
@@ -35,22 +34,9 @@ class FireSpreadingTest {
                     (NeighborhoodPattern) Class.forName("cellsociety.logic.neighborhoodpatterns." + metadata.get("Neighborhood"))
                             .getConstructor().newInstance(),
                     metadata);
-            for(int i = 0; i < fs.getGrid().getWidth(); i++){
-                for(int j = 0; j < fs.getGrid().getHeight(); j++){
-                    System.out.print(fs.getGrid().getCell(i,j).getCurrentState() + ",");
-                }
-                System.out.println("");
-            }
             fs.update();
             int x = 0;
             int y = 0;
-            for(int i = 0; i < fs.getGrid().getWidth(); i++){
-                for(int j = 0; j < fs.getGrid().getHeight(); j++){
-                    System.out.print(fs.getGrid().getCell(i,j).getCurrentState() + ",");
-                }
-                System.out.println("");
-            }
-
             assertEquals(1, fs.getGrid().getCell(x, y).getCurrentState());
             x = 4;
             y = 4;
@@ -68,7 +54,6 @@ class FireSpreadingTest {
             y = 5;
             assertEquals(2, fs.getGrid().getCell(x, y).getCurrentState());
         } catch (Exception e) {
-            e.printStackTrace();
             fail("File Not Found");
         }
 
@@ -78,11 +63,15 @@ class FireSpreadingTest {
     @Test
     void simulationOfDiagonalFireTest() {
         try {
-            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/fire_spreading/single_fire.sim"));
+            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/fire_spreading/diagonal_fire.sim"));
             metadata.put("ProbCatch", "1.00");
-            fs = (FireSpreading) getClass().getMethod(metadata.get("Type"), Grid.class, NeighborhoodPattern.class, Map.class).invoke(
-                    this,
-                    CSVFileReader.readFile(new File(metadata.get(INITIAL_STATE_FILE))),
+            Class cls[] = new Class[]{int[][].class, ShapeManager.class};
+            ShapeManager sm = (ShapeManager) Class.forName("cellsociety.logic.shapes." + metadata.get("Shape")).getConstructor().newInstance();
+            Object[] params = {CSVFileReader.readFile(new File(metadata.get("InitialStates"))),
+                    sm};
+            Grid grid = (Grid) Class.forName("cellsociety.logic.grid." + metadata.get("GridType")).getConstructor(cls).newInstance(params);
+            fs = (FireSpreading) FireSpreading.class.getConstructor(Grid.class, NeighborhoodPattern.class, Map.class).newInstance(
+                    grid,
                     (NeighborhoodPattern) Class.forName("cellsociety.logic.neighborhoodpatterns." + metadata.get("Neighborhood"))
                             .getConstructor().newInstance(),
                     metadata);
@@ -111,11 +100,15 @@ class FireSpreadingTest {
     @Test
     void simulationOfRingOfFireTest() {
         try {
-            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/fire_spreading/single_fire.sim"));
+            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/fire_spreading/ring_of_fire.sim"));
             metadata.put("ProbCatch", "1.00");
-            fs = (FireSpreading) getClass().getMethod(metadata.get("Type"), Grid.class, NeighborhoodPattern.class, Map.class).invoke(
-                    this,
-                    CSVFileReader.readFile(new File(metadata.get(INITIAL_STATE_FILE))),
+            Class cls[] = new Class[]{int[][].class, ShapeManager.class};
+            ShapeManager sm = (ShapeManager) Class.forName("cellsociety.logic.shapes." + metadata.get("Shape")).getConstructor().newInstance();
+            Object[] params = {CSVFileReader.readFile(new File(metadata.get("InitialStates"))),
+                    sm};
+            Grid grid = (Grid) Class.forName("cellsociety.logic.grid." + metadata.get("GridType")).getConstructor(cls).newInstance(params);
+            fs = (FireSpreading) FireSpreading.class.getConstructor(Grid.class, NeighborhoodPattern.class, Map.class).newInstance(
+                    grid,
                     (NeighborhoodPattern) Class.forName("cellsociety.logic.neighborhoodpatterns." + metadata.get("Neighborhood"))
                             .getConstructor().newInstance(),
                     metadata);
