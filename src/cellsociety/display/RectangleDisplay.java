43445ee8b5606112035bd1a05935100303940111
Tim Jang <timjang3@gmail.com>
2021-10-30 14:28:13 -0400

    rectangle display class implemented

--- src/cellsociety/display/RectangleDisplay.java
+++ src/cellsociety/display/RectangleDisplay.java
@@ -2,6 +2,7 @@ package cellsociety.display;
 
 import cellsociety.logic.grid.Cell;
 import cellsociety.logic.grid.Coordinate;
+import cellsociety.logic.grid.Grid;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -11,25 +12,57 @@ import java.util.List;
 import java.util.Map;
 
 public class RectangleDisplay extends Display{
-    Map<Coordinate, Rectangle> cellMap = new HashMap<>();
+    Map<Coordinate, Rectangle> cellMap;
 
     public RectangleDisplay(Stage stage, Color background){
         super(stage, background);
     }
 
     public void initializeGrid(List<Cell> cells){
+        resetGrid();
+
         if(cells.size() == 0) return;
 
         for(int i = 0; i < cells.size(); i++){
-            Rectangle cell = new Rectangle(cells.get(i).getCoordinates().r() * (cellLength + cellOffset) + gridLeftOffset,
+            resetGrid();
+
+            cellMap = new HashMap<>();
+            Rectangle rectCell = new Rectangle(cells.get(i).getCoordinates().r() * (cellLength + cellOffset) + gridLeftOffset,
                     cells.get(i).getCoordinates().c() * (cellOffset + cellLength) + gridTopOffset, cellLength, cellLength);
 
-            cellMap.put(cells.get(i).getCoordinates(), cell);
+            cellMap.put(cells.get(i).getCoordinates(), rectCell);
+            root.getChildren().add(rectCell);
         }
 
-
-
+        updateScene(cells);
     }
 
+    /**
+     * Removes all elements of the rectangle objects from the display.
+     */
+    public void resetGrid(){
+        if (cellMap != null) {
+            for (Coordinate coordinate : cellMap.keySet()) {
+                root.getChildren().remove(cellMap.get(coordinate));
+            }
+        }
+    }
+
+    /**
+     * Update Scene
+     * @param cells
+     */
+    public void updateScene(List<Cell> cells) {
+        if (cells == null || cells.size() == 0) {
+            return;
+        }
+
+        for (int i = 0; i < cells.size(); i++) {
+            Coordinate cellCoordinate = cells.get(i).getCoordinates();
+            int currentState = cells.get(i).getCurrentState();
+
+            cellMap.get(cellCoordinate).setFill(COLOR_MAP.get(currentState));
+        }
+    }
 
 }
