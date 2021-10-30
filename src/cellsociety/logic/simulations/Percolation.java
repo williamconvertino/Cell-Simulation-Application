9a2d3124503fe2b7c47828006eeb2335fcd7b4d4
alexis.cruzayala <adc94@duke.edu>
2021-10-30 18:25:35 -0400

    added new keys to the rest of the sim files and fixed percolation tests

--- src/cellsociety/logic/simulations/Percolation.java
+++ src/cellsociety/logic/simulations/Percolation.java
@@ -11,37 +11,39 @@ import java.util.Map;
 
 /**
  * @author Alexis Cruz-Ayala
- *
  * @since 0.0.2
  */
 public class Percolation extends Simulation {
 
-  /**
-   * Constructs a new Simulation with a specified starting Grid and a Map of simulation-specific data
-   * values.
-   *
-   * @param grid     the starting grid_LEGACY of the simulation.
-   * @param metadata the user-specified values used by the simulation.
-   * @throws MissingSimulationArgumentError if the metadata is missing a required argument for the
-   *                                        simulation.
-   */
-  public Percolation(Grid grid, NeighborhoodPattern np, Map<String, String> metadata)
-          throws MissingSimulationArgumentError {
-    super(grid, np, metadata);
+    /**
+     * Constructs a new Simulation with a specified starting Grid and a Map of simulation-specific data
+     * values.
+     *
+     * @param grid     the starting grid_LEGACY of the simulation.
+     * @param metadata the user-specified values used by the simulation.
+     * @throws MissingSimulationArgumentError if the metadata is missing a required argument for the
+     *                                        simulation.
+     */
+    public Percolation(Grid grid, NeighborhoodPattern np, Map<String, String> metadata)
+            throws MissingSimulationArgumentError {
+        super(grid, np, metadata);
 //    setDefaultValue(0);
-  }
-
-  @Override
-  protected void updateNextGridFromCell(Cell cell) {
-
-    List<Cell> neighbors = getGrid().getNeighbors(cell, getNeighborhoodPattern());
-
-    if ((neighbors.get(0).getCurrentState() == 1 || cell.getCurrentState() == 0 ) && cell.getCurrentState() == 0) {
-      getGrid().changeCell(cell, 1);
-    }  else{
-      getGrid().changeCell(cell, cell.getCurrentState());
     }
 
-  }
+    @Override
+    protected void updateNextGridFromCell(Cell cell) {
+
+        List<Cell> neighbors = getGrid().getNeighbors(cell, getNeighborhoodPattern());
+        if(neighbors.get(1) == null && cell.getCurrentState() == 0){
+            getGrid().changeCell(cell, 1);
+        } else if(neighbors.get(1) != null){
+            if ((neighbors.get(1).getCurrentState() == 1 || cell.getCurrentState() == 0) && cell.getCurrentState() == 0) {
+                getGrid().changeCell(cell, 1);
+            } else {
+                getGrid().changeCell(cell, cell.getCurrentState());
+            }
+        }
+
+    }
 
 }
--- test/cellsociety/logic/simulations/PercolationTest.java
+++ test/cellsociety/logic/simulations/PercolationTest.java
@@ -1,80 +1,111 @@
-//package cellsociety.logic.simulations;
-//
-//import cellsociety.io.CSVFileReader;
-//import cellsociety.io.SIMFileReader;
-//import cellsociety.logic.grid_LEGACY.Grid;
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.File;
-//import java.util.HashMap;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class PercolationTest {
-//    private Percolation p;
-//    @Test
-//    void simplePipeTest() {
-//        try {
-//            Grid grid = new Grid(CSVFileReader.readFile(new File("data/percolation/simple_pipe.csv")));
-//            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/percolation/simple_pipe.sim"));
-//            p = new Percolation(grid.getCellStates(), metadata);
-//            p.update();
-//            int x = 0;
-//            int y = 0;
-//            assertEquals(2, p.getGrid().getCell(x, y).getState());
-//            x = 0;
-//            y = 2;
-//            assertEquals(1, p.getGrid().getCell(x, y).getState());
-//            x = 1;
-//            y = 2;
-//            assertEquals(1, p.getGrid().getCell(x, y).getState());
-//        } catch (Exception e) {
-//            fail("File Not Found");
-//        }
-//
-//    }
-//
-//    @Test
-//    void longPipeTest() {
-//        try {
-//            Grid grid = new Grid(CSVFileReader.readFile(new File("data/percolation/long_pipe.csv")));
-//            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/percolation/long_pipe.sim"));
-//            p = new Percolation(grid.getCellStates(), metadata);
-//            p.update();
-//            int x = 0;
-//            int y = 0;
-//            assertEquals(1, p.getGrid().getCell(x, y).getState());
-//            x = 0;
-//            y = 2;
-//            assertEquals(1, p.getGrid().getCell(x, y).getState());
-//            x = 1;
-//            y = 2;
-//            assertEquals(1, p.getGrid().getCell(x, y).getState());
-//        } catch (Exception e) {
-//            fail("File Not Found");
-//        }
-//
-//    }
-//
-//    @Test
-//    void volcanoTest() {
-//        try {
-//            Grid grid = new Grid(CSVFileReader.readFile(new File("data/percolation/volcano.csv")));
-//            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/percolation/volcano.sim"));
-//            p = new Percolation(grid.getCellStates(), metadata);
-//            p.update();
-//            int x = 0;
-//            int y = 0;
-//            assertEquals(1, p.getGrid().getCell(x, y).getState());
-//            x = 0;
-//            y = 2;
-//            assertEquals(1, p.getGrid().getCell(x, y).getState());
-//            x = 1;
-//            y = 5;
-//            assertEquals(2, p.getGrid().getCell(x, y).getState());
-//        } catch (Exception e) {
-//            fail("File Not Found");
-//        }
-//
-//    }
-//}
+package cellsociety.logic.simulations;
+
+import cellsociety.io.CSVFileReader;
+import cellsociety.io.SIMFileReader;
+import cellsociety.logic.grid.Grid;
+import cellsociety.logic.neighborhoodpatterns.NeighborhoodPattern;
+import cellsociety.logic.shapes.ShapeManager;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PercolationTest {
+    private Percolation p;
+    @Test
+    void simplePipeTest() {
+        try {
+            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/percolation/simple_pipe.sim"));
+            metadata.put("ProbCatch", "1.00");
+            Class cls[] = new Class[]{int[][].class, ShapeManager.class};
+            ShapeManager sm = (ShapeManager) Class.forName("cellsociety.logic.shapes." + metadata.get("Shape")).getConstructor().newInstance();
+            Object[] params = {CSVFileReader.readFile(new File(metadata.get("InitialStates"))),
+                    sm};
+            Grid grid = (Grid) Class.forName("cellsociety.logic.grid." + metadata.get("GridType")).getConstructor(cls).newInstance(params);
+            p = (Percolation) Percolation.class.getConstructor(Grid.class, NeighborhoodPattern.class, Map.class).newInstance(
+                    grid,
+                    (NeighborhoodPattern) Class.forName("cellsociety.logic.neighborhoodpatterns." + metadata.get("Neighborhood"))
+                            .getConstructor().newInstance(),
+                    metadata);
+            p.update();
+            int x = 0;
+            int y = 0;
+            assertEquals(2, p.getGrid().getCell(x, y).getCurrentState());
+            x = 0;
+            y = 2;
+            assertEquals(1, p.getGrid().getCell(x, y).getCurrentState());
+            x = 1;
+            y = 2;
+            assertEquals(1, p.getGrid().getCell(x, y).getCurrentState());
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("File Not Found");
+        }
+
+    }
+
+    @Test
+    void longPipeTest() {
+        try {
+            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/percolation/long_pipe.sim"));
+            metadata.put("ProbCatch", "1.00");
+            Class cls[] = new Class[]{int[][].class, ShapeManager.class};
+            ShapeManager sm = (ShapeManager) Class.forName("cellsociety.logic.shapes." + metadata.get("Shape")).getConstructor().newInstance();
+            Object[] params = {CSVFileReader.readFile(new File(metadata.get("InitialStates"))),
+                    sm};
+            Grid grid = (Grid) Class.forName("cellsociety.logic.grid." + metadata.get("GridType")).getConstructor(cls).newInstance(params);
+            p = (Percolation) Percolation.class.getConstructor(Grid.class, NeighborhoodPattern.class, Map.class).newInstance(
+                    grid,
+                    (NeighborhoodPattern) Class.forName("cellsociety.logic.neighborhoodpatterns." + metadata.get("Neighborhood"))
+                            .getConstructor().newInstance(),
+                    metadata);
+            p.update();
+            int x = 0;
+            int y = 0;
+            assertEquals(1, p.getGrid().getCell(x, y).getCurrentState());
+            x = 0;
+            y = 2;
+            assertEquals(1, p.getGrid().getCell(x, y).getCurrentState());
+            x = 1;
+            y = 2;
+            assertEquals(1, p.getGrid().getCell(x, y).getCurrentState());
+        } catch (Exception e) {
+            fail("File Not Found");
+        }
+
+    }
+
+    @Test
+    void volcanoTest() {
+        try {
+            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/percolation/volcano.sim"));
+            metadata.put("ProbCatch", "1.00");
+            Class cls[] = new Class[]{int[][].class, ShapeManager.class};
+            ShapeManager sm = (ShapeManager) Class.forName("cellsociety.logic.shapes." + metadata.get("Shape")).getConstructor().newInstance();
+            Object[] params = {CSVFileReader.readFile(new File(metadata.get("InitialStates"))),
+                    sm};
+            Grid grid = (Grid) Class.forName("cellsociety.logic.grid." + metadata.get("GridType")).getConstructor(cls).newInstance(params);
+            p = (Percolation) Percolation.class.getConstructor(Grid.class, NeighborhoodPattern.class, Map.class).newInstance(
+                    grid,
+                    (NeighborhoodPattern) Class.forName("cellsociety.logic.neighborhoodpatterns." + metadata.get("Neighborhood"))
+                            .getConstructor().newInstance(),
+                    metadata);
+            p.update();
+            int x = 0;
+            int y = 0;
+            assertEquals(1, p.getGrid().getCell(x, y).getCurrentState());
+            x = 0;
+            y = 2;
+            assertEquals(1, p.getGrid().getCell(x, y).getCurrentState());
+            x = 1;
+            y = 5;
+            assertEquals(2, p.getGrid().getCell(x, y).getCurrentState());
+        } catch (Exception e) {
+            fail("File Not Found");
+        }
+
+    }
+}
