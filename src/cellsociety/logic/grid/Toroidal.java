35c4da4bc1c7c48516c30193085a366a876e0795
alexis.cruzayala <adc94@duke.edu>
2021-10-31 20:46:22 -0400

    added toirodal grid

---
+++ src/cellsociety/logic/grid/Toroidal.java
@@ -0,0 +1,36 @@
+package cellsociety.logic.grid;
+
+import cellsociety.logic.neighborhoodpatterns.NeighborhoodPattern;
+import cellsociety.logic.shapes.ShapeManager;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Toroidal extends Grid{
+
+
+
+    /**
+     * Constructs a new grid with a specified array of states
+     *
+     * @param states
+     * @param shapeManager
+     */
+    public Toroidal(int[][] states, ShapeManager shapeManager) {
+        super(states, shapeManager);
+    }
+
+    @Override
+    public List<Cell> getNeighbors(Cell cell, NeighborhoodPattern myPattern) {
+        List<Coordinate> potentialNeighbors = generateNeighborCoordinates(cell, myPattern);
+        List<Cell> myNeighbors = new LinkedList<>();
+        for (Coordinate coord: potentialNeighbors) {
+            if(coord.r() < 0 || coord.r() > getHeight() || coord.c() < 0 || coord.c() > getWidth()){
+                myNeighbors.add(getCell(coord.r()%getWidth(), coord.c()%getHeight()));
+            }else{
+                myNeighbors.add(getCell(coord.r(), coord.c()));
+            }
+        }
+        return myNeighbors;
+    }
+}
