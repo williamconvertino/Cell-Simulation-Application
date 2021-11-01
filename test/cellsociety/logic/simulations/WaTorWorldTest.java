51cb02f5cbeb72e13387c3f3d66a222ff5ebc902
Quentin MacFarlane <qtm@duke.edu>
2021-11-01 17:13:01 -0400

    fixed watorworld tests and updated some wator world data files

--- test/cellsociety/logic/simulations/WaTorWorldTest.java
+++ test/cellsociety/logic/simulations/WaTorWorldTest.java
@@ -16,16 +16,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WaTorWorldTest {
     private WaTorWorld ww;
+
     @Test
-    void simplePipeTest() {
+    void randomAssortmentTest() {
         try {
             HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/wator/random_assortment.sim"));
             Class cls[] = new Class[]{int[][].class, ShapeManager.class};
-            ShapeManager sm = (ShapeManager) Class.forName("cellsociety.logic.shapes." + metadata.get("Shape")).getConstructor().newInstance();
-            Object[] params = {CSVFileReader.readFile(new File(metadata.get(INITIAL_STATE_FILE))),
+            ShapeManager sm = (ShapeManager) Class.forName("cellsociety.logic.shapes." + metadata.get("Shape") + "ShapeManager").getConstructor().newInstance();
+            Object[] params = {CSVFileReader.readFile(new File(metadata.get("InitialStates"))),
                     sm};
             Grid grid = (Grid) Class.forName("cellsociety.logic.grid." + metadata.get("GridType")).getConstructor(cls).newInstance(params);
-
             ww = (WaTorWorld) WaTorWorld.class.getConstructor(Grid.class, NeighborhoodPattern.class, Map.class).newInstance(
                     grid,
                     (NeighborhoodPattern) Class.forName("cellsociety.logic.neighborhoodpatterns." + metadata.get("Neighborhood"))
@@ -48,46 +48,72 @@ class WaTorWorldTest {
 
     }
 
-//    @Test
-//    void longPipeTest() {
-//        try {
-//            Grid grid = new Grid(CSVFileReader.readFile(new File("data/wator/single_fish.csv")));
-//            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/wator/single_fish.sim"));
-//            ww = new WaTorWorld(grid.getCellStates(), metadata);
-//            ww.update();
-//            int x = 0;
-//            int y = 0;
-//            assertEquals(1, ww.getGrid().getCell(x, y).getState());
-//            x = 0;
-//            y = 2;
-//            assertEquals(1, ww.getGrid().getCell(x, y).getState());
-//            x = 1;
-//            y = 2;
-//            assertEquals(1, ww.getGrid().getCell(x, y).getState());
-//        } catch (Exception e) {
-//            fail("File Not Found");
-//        }
-//
-//    }
-//
-//    @Test
-//    void volcanoTest() {
-//        try {
-//            Grid grid = new Grid(CSVFileReader.readFile(new File("data/wator/random_assortment.csv")));
-//            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/wator/random_assortment.sim"));
-//            ww = new WaTorWorld(grid.getCellStates(), metadata);
-//            ww.update();
-//            int x = 0;
-//            int y = 0;
-//            assertEquals(1, ww.getGrid().getCell(x, y).getState());
-//            x = 0;
-//            y = 2;
-//            assertEquals(1, ww.getGrid().getCell(x, y).getState());
-//            x = 1;
-//            y = 5;
-//            assertEquals(2, ww.getGrid().getCell(x, y).getState());
-//        } catch (Exception e) {
-//            fail("File Not Found");
-//        }
+    @Test
+    void bigOceanTest() {
+        try {
+            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/wator/big_ocean.sim"));
+            Class cls[] = new Class[]{int[][].class, ShapeManager.class};
+            ShapeManager sm = (ShapeManager) Class.forName("cellsociety.logic.shapes." + metadata.get("Shape") + "ShapeManager").getConstructor().newInstance();
+            Object[] params = {CSVFileReader.readFile(new File(metadata.get("InitialStates"))),
+                    sm};
+            Grid grid = (Grid) Class.forName("cellsociety.logic.grid." + metadata.get("GridType")).getConstructor(cls).newInstance(params);
+            ww = (WaTorWorld) WaTorWorld.class.getConstructor(Grid.class, NeighborhoodPattern.class, Map.class).newInstance(
+                    grid,
+                    (NeighborhoodPattern) Class.forName("cellsociety.logic.neighborhoodpatterns." + metadata.get("Neighborhood"))
+                            .getConstructor().newInstance(),
+                    metadata);
+            ww.update();
+            int x = 0;
+            int y = 0;
+            assertEquals(0, ww.getGrid().getCell(x, y).getCurrentState());
+            x = 0;
+            y = 2;
+            assertEquals(0, ww.getGrid().getCell(x, y).getCurrentState());
+            x = 11;
+            y = 11;
+            assertEquals(0, ww.getGrid().getCell(x, y).getCurrentState());
+        } catch (Exception e) {
+            fail("File Not Found");
+        }
 
     }
+
+    @Test
+    void volcanoTest() {
+        try {
+            HashMap<String, String> metadata = (HashMap<String, String>) SIMFileReader.getMetadataFromFile(new File("data/wator/divided.sim"));
+            Class cls[] = new Class[]{int[][].class, ShapeManager.class};
+            ShapeManager sm = (ShapeManager) Class.forName("cellsociety.logic.shapes." + metadata.get("Shape") + "ShapeManager").getConstructor().newInstance();
+            Object[] params = {CSVFileReader.readFile(new File(metadata.get("InitialStates"))),
+                    sm};
+            Grid grid = (Grid) Class.forName("cellsociety.logic.grid." + metadata.get("GridType")).getConstructor(cls).newInstance(params);
+            ww = (WaTorWorld) WaTorWorld.class.getConstructor(Grid.class, NeighborhoodPattern.class, Map.class).newInstance(
+                    grid,
+                    (NeighborhoodPattern) Class.forName("cellsociety.logic.neighborhoodpatterns." + metadata.get("Neighborhood"))
+                            .getConstructor().newInstance(),
+                    metadata);
+            ww.update();
+            ww.update();
+            ww.update();
+            ww.update();
+            ww.update();
+            ww.update();
+            ww.update();
+            ww.update();
+            ww.update();
+            ww.update();
+            int x;
+            int y;
+            for (int j=0; j<5; j++) {
+                for (int k=0; k<5; k++) {
+                    x=j;
+                    y=k;
+                    assertEquals(0, ww.getGrid().getCell(x, y).getCurrentState());
+                }
+            }
+        } catch (Exception e) {
+            fail("File Not Found");
+        }
+
+    }
+}
